fa6d82616fbd6c0ae24d5bff8a2df3513b6c70b5
Rivaldo Fernandes <rivaldofez@gmail.com>
2024-11-28 18:52:45 +0700

    Change text main activity to Hello World!

--- app/src/main/java/com/rivaldofez/exampleprojectgit/MainActivity.kt
+++ app/src/main/java/com/rivaldofez/exampleprojectgit/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ExampleProjectGitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello World!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
